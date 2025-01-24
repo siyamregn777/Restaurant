63dbe3dd68275827e433832be10b751484e2d855
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-01-25 02:03:49 +0300

    add page for footer

---
+++ my-frontend/src/pages/AboveFoot.jsx
@@ -0,0 +1,24 @@
+import  './AboveFoot.css'
+
+function AboveFoot() {
+    return(
+        <div className="above-foot">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et maiores, fuga, voluptas 
+                esse deleniti, delectus iste consequuntur laudantium in nobis praesentium nemo iust
+                o eius repellendus. Nesciunt, ducimus. Numquam, eos accusantium. Aperiam consequunt
+                ur magnam amet. Corporis nesciunt temporibus asperiores voluptate quod a sunt! Adip
+                isci saepe quibusdam perferendis recusandae omnisodit consequuntur obcaecati magnam
+                voluptatem sit aperiam similique officiis id, quam culpa!Nihil est nemo tempore dolo
+                res perferendis repudiandae consequatur velit quidem quos voluptate corruptilibero e
+                um totam optio rerum repellendus impedit, deleniti tempora vero. Pariatur animi dele
+                ctus, laudantium eos illum consectetur? Placeat distinctio ducimus harum eius odit at
+                numquam accusantium, facere dolore repellendus voluptatibus modi id possimus iure adi
+                pisci esse in voluptas tenetur. Alias voluptates distinctio facilis suscipit omnis, s
+                unt harum?
+        </p>
+        </div>
+
+    );
+
+}
+export default AboveFoot;
