c8311c578957ea4967894adb9111bc7253e3a51b
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-01-29 13:50:21 +0300

    book BookManagement page that helps manae the available books

---
+++ my-frontend/src/pages/BookManagement.jsx
@@ -0,0 +1,10 @@
+function BookManagement() {
+    return (
+        <div>
+            <h1>Manage Books</h1>
+            {/* Components for CRUD operations on books */}
+        </div>
+    );
+}
+
+export default BookManagement;
